ea3cd5fdcd1ac87e34bc2f635ff622b6932772c7
Lukas <lukatran@ksr.ch>
2022-03-12 23:22:37 +0100

    Steuerung buggy, Bat funktioniert nicht

--- dino_game_i.py
+++ dino_game_i.py
@@ -1,96 +1,76 @@
 from microbit import *
 import random
 
-def pix_up(x, y):
-    display.set_pixel(x, y, 9)
-    display.set_pixel(x-1, y-1, 0)
-
-def pix_down(x, y):
-    display.set_pixel(x, y, 9)
-    display.set_pixel(x+1, y+1, 0)
-
-y = 0
-time = 500
-a = 0
+jump = 0
+limiter_c_or_b = 0
+time = 800
+cactus_x_i = 4
+bat_x_i = 4
+dino_i = 3
+dino_ii = 2
+jump_time = 0
+cactus_or_bat = 0
+bat_i_or_bat_ii = 0
 
 while True:
 
     #Dino
-    #Sprung
+    #Jump
     if button_a.get_presses():
+        jump_time = running_time()
         display.set_pixel(0, 2, 9)
-        sleep(time)
-        display.set_pixel(0, 2, 0)
-    #Ducken
-    if button_b.is_pressed():
+        display.set_pixel(0, 4, 0)
+        dino_i = 2
+        dino_ii = 3
+        #Jump_time_Start
+        jump = 1
+    #Crouch
+    elif button_b.is_pressed():
         display.set_pixel(0, 3, 0)
         display.set_pixel(0, 4, 9)
-    #Laufen
+        dino_i = 4
+        dino_ii = 4
+        jump = 3
     else:
+    #Run
         display.set_pixel(0, 3, 9)
         display.set_pixel(0, 4, 9)
+        dino_i = 3
+        dino_ii = 4
+    #Jump_time_Stop
+    if jump == 1 and jump_time + time * 1.5 == running_time():
+        jump = 0
+        display.set_pixel(0, 2, 0)
 
-    #Kaktus
-    display.set_pixel(4, 4, 5)
+    if cactus_or_bat == 0:
+        #Cactus
+        if running_time() % time == 0:
+            display.set_pixel(cactus_x_i, 4, 5)
+            if cactus_x_i < 4:
+                display.set_pixel(cactus_x_i+1, 4, 0)
+            cactus_x_i -= 1
+        if cactus_x_i == -1:
+            cactus_x_i = 4
+            cactus_or_bat = random.randint(-1, 2)
 
-    #sleep
-    while a <= time:
-        #Dino
-        #Sprung
-        if button_a.get_presses():
-            display.set_pixel(0, 2, 9)
-            display.set_pixel(0, 3, 9)
-            display.set_pixel(0, 4, 0)
-            sleep(time)
-            display.set_pixel(0, 2, 0)
-            a += 1
-            sleep(1)
-        #Ducken
-        if button_b.is_pressed():
-            display.set_pixel(0, 3, 0)
-            display.set_pixel(0, 4, 9)
-            a += 1
-            sleep(1)
-        #Laufen
-        else:
-            display.set_pixel(0, 3, 9)
-            display.set_pixel(0, 4, 9)
-            a += 1
-            sleep(1)
-    a = 0
+    if cactus_or_bat == 1:
+        #Bat
+        if running_time() % time == 0:
+            display.set_pixel(bat_x_i, 3, 5)
+            if bat_x_i < 4:
+                display.set_pixel(bat_x_i+1, 3, 0)
+            bat_x_i -= 1
+        if bat_x_i == -1:
+            bat_x_i = 4
+            cactus_or_bat = random.randint(-1, 2)
 
-    display.set_pixel(4, 4, 0)
-    for i in range(3, -1, -1):
-        display.set_pixel(i, 4, 5)
-        display.set_pixel(i + 1, 4, 0)
+    #Easy-Time
+    if running_time() < 20000:
+        cactus_or_bat = 0
 
-        #sleep
-        while a <= time:
-            #Dino
-            #Sprung
-            if button_a.get_presses():
-                display.set_pixel(0, 2, 9)
-                display.set_pixel(0, 3, 9)
-                display.set_pixel(0, 4, 0)
-                sleep(time)
-                display.set_pixel(0, 2, 0)
-                a += 1
-                sleep(1)
-
-            #Ducken
-            if button_b.is_pressed():
-                display.set_pixel(0, 3, 0)
-                display.set_pixel(0, 4, 9)
-                a += 1
-                sleep(1)
-
-            #Laufen
-            else:
-                display.set_pixel(0, 3, 9)
-                display.set_pixel(0, 4, 9)
-                a += 1
-                sleep(1)
-        a = 0
-    display.set_pixel(0, 4, 0)
-
+    """
+    if cactus_x_i == dino_i or cactus_x_i == dino_ii or bat_x_i == dino_i or bat_x_i == dino_ii:
+        display.scroll("Game Over")
+    """
+    sleep(1)
 #BACKUP
