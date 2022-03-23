db4f16c555f9561512cbd280669d9475e6c3cad7
Lukas <lukatran@ksr.ch>
2022-03-23 16:52:39 +0100

    clear function added

---
+++ useful_functions.py
@@ -0,0 +1,5 @@
+def clear():
+## CLEARS CONSOLE
+## NOTE that there are different commands for Windows and Unix-based system (Mac and Linux)
+    if os.name == 'nt': os.system('cls') # Windows
+    else: os.system('clear') # Unix
