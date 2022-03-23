448c23efa77d8641012f74c2e8b23bec28705134
Lukas <lukatran@ksr.ch>
2022-03-23 15:08:05 +0100

    rough code idea

--- space_war_ii_model_view.py
+++ space_war_ii_model_view.py
@@ -1,6 +1,7 @@
-### IMPORT MODULES
-
-### GAME PARAMETERS
+### IMPORT MODULE
+import random
+ 
+## GAME PARAMETERS
 # State parameters of game such as WIDTH or HEIGHT of matrix.
 # Use capital letters to indicate that this are parameter with fixed values instead of variables!
 
@@ -18,9 +19,27 @@
 # When porting you game from e.g. normal python (console app) to mirco:bit, only these functions have to be adapted/rewritten
 
 
+
+
+def add_random_asteroids(L):
+    asteroid_x = random.randint(-1, 5)
+    L.append([asteroid_x, 0])
+    return L
+
+def move_and_delete_asteroids(L):
+    pass
+
+Asteroids = []
+frame = 40 #Time one frame is shown
+fps = 50
+
 ### GAME LOOP
 
-while True:    
+##while True:
+add_random_asteroids(Asteroids)
+add_random_asteroids(Asteroids)
+print(Asteroids)
+print(Asteroids[1])
     ### UPDATE MODEL
     # here call model functions to update 
     
