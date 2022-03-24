546e209c736e6a3014fb2963b79e2be8b15eac00
Lukas <lukatran@ksr.ch>
2022-03-24 17:51:45 +0100

    completed

--- space_war_ii_model_view.py
+++ space_war_ii_model_view.py
@@ -1,45 +1,47 @@
 ### IMPORT MODULE
 import random
+import os
  
 ## GAME PARAMETERS
 # State parameters of game such as WIDTH or HEIGHT of matrix.
 # Use capital letters to indicate that this are parameter with fixed values instead of variables!
-
+WIDTH = 8
+Asteroids = []
+frame = 40 #Time one frame is shown
+fps = 50
 
 ### UTILITY FUNCTIONS
 # utility functions that are of general use, e.g. to manipulate strings
-
+def clear():
+## CLEARS CONSOLE
+## NOTE that there are different commands for Windows and Unix-based system (Mac and Linux)
+    if os.name == 'nt': os.system('cls') # Windows
+    else: os.system('clear') # Unix
 
 ### MODEL FUNCTIONS
 # here write all functions that belong to the game's model/logic
+def add_random_asteroids(L, probability, WIDTH):
+    for i in range(WIDTH):
+        rate = random.randint(0, 100)
+        if rate <= probability:
+            asteroid_x = i
+            L.append([asteroid_x, 0])
+    return L
+
+def move_and_delete_asteroids(L):
+    pass
 
 
 ### VIEW FUNCTIONS
 # Here write all functions that belong to the game's view.
 # When porting you game from e.g. normal python (console app) to mirco:bit, only these functions have to be adapted/rewritten
 
-
-
-
-def add_random_asteroids(L):
-    asteroid_x = random.randint(-1, 5)
-    L.append([asteroid_x, 0])
-    return L
-
-def move_and_delete_asteroids(L):
-    pass
-
-Asteroids = []
-frame = 40 #Time one frame is shown
-fps = 50
-
 ### GAME LOOP
 
 ##while True:
-add_random_asteroids(Asteroids)
-add_random_asteroids(Asteroids)
+add_random_asteroids(Asteroids, 10, WIDTH)
 print(Asteroids)
-print(Asteroids[1])
+##print(Asteroids[1])
     ### UPDATE MODEL
     # here call model functions to update 
     
