a0214ce33404847cc303cb58c674a03e9444b429
Lukas <lukatran@ksr.ch>
2022-03-26 23:41:05 +0100

    ZeroDivisionError, idk why

--- space_war_i.py
+++ space_war_i.py
@@ -1,114 +1,89 @@
 from microbit import *
 import random
 
-a = 2
-b = 0
-c = 0
-d = 0
-i = 0
-x = False
+space_ship_x = 2
+space_ship_x_ii = 0
+asteroid_x = 0
+asteroid_y = 0
+asteroid_count = 0
+first = 1
+frame_time_ms_ii = 0
+frame_time_single = 40
+game_status = False
 time = 400
-count = 0
+score = 0
 level = 1
+
+def space_ship_move(space_ship_x, indicator):
+    if indicator == 1:
+        space_ship_x -= 1
+        if space_ship_x < 0:
+            space_ship_x = 4
+    else:
+        space_ship_x += 1
+        if space_ship_x > 4:
+            space_ship_x = 0
+    return space_ship_x
+
+def add_asteroid():
+    pass
 
 while True:
     if pin_logo.is_touched():
-        x = True
-    while x:
+        game_status = True
+    while game_status:
 
         # Spaceship
 
-        display.set_pixel(a, 4, 9)
+        display.set_pixel(space_ship_x, 4, 9)
 
         if button_a.get_presses():
-            b = a
-            a -= 1
-            if a < 0:
-                a = 4
-            display.set_pixel(a, 4, 9)
-            display.set_pixel(b, 4, 0)
+            space_ship_x_ii = space_ship_x
+            space_ship_x = space_ship_move(space_ship_x, 1)
+            display.set_pixel(space_ship_x, 4, 9)
+            display.set_pixel(space_ship_x_ii, 4, 0)
 
         if button_b.get_presses():
-            b = a
-            a += 1
-            if a > 4:
-                a = 0
-            display.set_pixel(a, 4, 9)
-            display.set_pixel(b, 4, 0)
+            space_ship_x_ii = space_ship_x
+            space_ship_x = space_ship_move(space_ship_x, -1)
+            display.set_pixel(space_ship_x, 4, 9)
+            display.set_pixel(space_ship_x_ii, 4, 0)
 
-        # Asteroiden
+        #Asteroids
 
-        c = random.randint(0, 4)
+        frame_time_ms = running_time()
+        if frame_time_ms == 0:
+            frame_time_ms = 1
 
-        display.set_pixel(c, 0, 3)
+        if frame_time_ms % time == 0:
 
-        x = 0
+            asteroid_x = random.randint(0, 4)
+            asteroid_count += 1
 
-        while x <= time:
+            if not first == 1:
+                asteroid_y += 1
+                if asteroid_y == 5:
+                    asteroid_y = 0
 
-            sleep(1)
+                display.set_pixel(asteroid_x, asteroid_y, 3)
+                display.set_pixel(asteroid_x, asteroid_y, 0)
+                first = 0
 
-            if button_a.get_presses():
-                b = a
-                a -= 1
-                if a < 0:
-                    a = 4
-                display.set_pixel(a, 4, 9)
-                display.set_pixel(b, 4, 0)
-
-            if button_b.get_presses():
-                b = a
-                a += 1
-                if a > 4:
-                    a = 0
-                display.set_pixel(a, 4, 9)
-                display.set_pixel(b, 4, 0)
-
-            x += 1
-
-        for i in range(1, 5, 1):
-            display.set_pixel(c, i, 3)
-            d = i
-            d -= 1
-            display.set_pixel(c, i, 3)
-            display.set_pixel(c, d, 0)
-
-            x = 0
-
-            while x <= time:
-
-                sleep(1)
-
-                if button_a.get_presses():
-                    b = a
-                    a -= 1
-                    if a < 0:
-                        a = 4
-                    display.set_pixel(a, 4, 9)
-                    display.set_pixel(b, 4, 0)
-
-                if button_b.get_presses():
-                    b = a
-                    a += 1
-                    if a > 4:
-                        a = 0
-                    display.set_pixel(a, 4, 9)
-                    display.set_pixel(b, 4, 0)
-
-                x += 1
-        count = count + 1
-        if count%10 == 0:
+        score = score + 1
+        if score % 10 == 0:
             level = level + 1
             time = time - 20
 
-        display.set_pixel(c, 4, 0)
+        frame_time_ms_ii = running_time()
+        if frame_time_ms_ii - frame_time_ms < frame_time_single:
+            sleep(frame_time_ms_ii - frame_time_ms)
 
         # Ende
 
-        if a == c and i == 4:
+        if space_ship_x == asteroid_x and asteroid_y == 4:
             display.clear()
             display.scroll("Game Over")
             level = 1
             time = 400
-            count = 0
-            x = False
+            score = 0
+            game_status = False
