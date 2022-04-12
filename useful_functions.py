a99929ae0f32eade75f389a2ac88fd1f6e8df123
Lukas <lukatran@ksr.ch>
2022-04-12 14:46:41 +0200

    nothing new

--- useful_functions.py
+++ useful_functions.py
@@ -1,5 +1,9 @@
+import os
+
 def clear():
 ## CLEARS CONSOLE
 ## NOTE that there are different commands for Windows and Unix-based system (Mac and Linux)
     if os.name == 'nt': os.system('cls') # Windows
     else: os.system('clear') # Unix
+
+print(os.name)
