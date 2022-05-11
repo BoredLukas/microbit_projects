5015c2b02485a7971405652724de65b505dddd05
Lukas <lukatran@ksr.ch>
2022-05-11 16:36:55 +0200

    def in def, now getting multiple cycles in

--- way_to_bahnhof.py
+++ way_to_bahnhof.py
@@ -7,24 +7,34 @@ graph = {
     "bahnhof": {},
 }
 
-START = "ksr"
-END = "bahnhof"
-SEARCH = True
-way_to_bahnhof = ["ksr"]
+way_to_bahnhof = [["ksr"]]
+visited = ["ksr"]
 
 def connection_check(graph, n1, n2):
     x1 = n2 in graph[n1]
     return x1
 
-def find_way(given_node):
-    for node in graph[given_node]:
-        way_to_bahnhof.append(node)
-        return node
+def find_way(graph, way_to_bahnhof, visited):
+    def node_append(given_node):
+        for node in graph[given_node]:
+            way_to_bahnhof[0].append(node)
+            return node
 
-node = find_way(START)
-while SEARCH:
-    last_node = node
-    node = find_way(node)
-    if node == "bahnhof":
-        SEARCH = False
-        print(way_to_bahnhof)
+    START = "ksr"
+    END = "bahnhof"
+    SEARCH = True
+
+    node = node_append(START)
+    visited.append(node)
+    while SEARCH:
+        last_node = node
+        node = node_append(node)
+        visited.append(node) 
+        if node == "bahnhof":
+            SEARCH = False
+            print(way_to_bahnhof)
+            print(visited)
+    return way_to_bahnhof
+
+cycles = find_way(graph, way_to_bahnhof, visited)
+print(cycles)
