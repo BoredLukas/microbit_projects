a6f8e7db7468bb77ae13b5c8058179b3e0e140da
Lukas <lukatran@ksr.ch>
2022-05-25 15:51:52 +0200

    failed attempt

--- way_to_bahnhof.py
+++ way_to_bahnhof.py
@@ -15,25 +15,30 @@ def connection_check(graph, n1, n2):
     return x1
 
 def find_way(graph, way_to_bahnhof, visited):
-    def node_append(given_node):
+    def node_append_first_list(given_node,list):
         for node in graph[given_node]:
-            way_to_bahnhof[0].append(node)
+            list[0].append(node)
             return node
+
+    def find_other_ways():
+        pass
 
     START = "ksr"
     END = "bahnhof"
     SEARCH = True
 
-    node = node_append(START)
+    node = node_append_first_list(START, way_to_bahnhof)
     visited.append(node)
     while SEARCH:
         last_node = node
-        node = node_append(node)
+        node = node_append_first_list(node, way_to_bahnhof)
+        # index = way_to_bahnhof.index(last_node)
         visited.append(node) 
-        if node == "bahnhof":
+        if node == END:
             SEARCH = False
             print(way_to_bahnhof)
             print(visited)
+            # print(index)
     return way_to_bahnhof
 
 cycles = find_way(graph, way_to_bahnhof, visited)
