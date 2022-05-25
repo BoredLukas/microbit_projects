b77efbf365bd8afb283d12f9ff1d7a1a702a5719
Lukas <lukatran@ksr.ch>
2022-05-25 15:52:18 +0200

    new attempt

---
+++ way_to_bahnhof_iii.py
@@ -0,0 +1,49 @@
+
+from tabnanny import check
+
+
+def pop_and_refresh_candidates(candidates):
+    check = candidates.pop(0)
+    for ways in graph[check]:
+        candidates.append(ways)
+
+graph = {
+    "ksr": {"sek": 1},
+    "sek": {"bahnhofstr": 3, "hafenstr": 5, "zelgstr": 7},
+    "bahnhofstr": {"bahnhof": 7},
+    "hafenstr": {"bahnhof": 6},
+    "zelgstr": {"bahnhof": 5},
+    "bahnhof": {},
+}
+
+candidates = ["ksr"]
+wtBahnhof = []
+visited = {}
+start = "ksr"
+
+while candidates:
+    check = candidates.pop(0)
+    visited[check]=start
+    for ways in graph[check]:
+        if ways in visited:
+            pass
+        else:
+            candidates.append(ways)
+        print(candidates)
+    start = check
+    print(visited)
+
+
+
+'''
+for places in graph:
+    for ways in graph[places]:
+        wtBahnhof.append(ways)
+        visited[ways]=start        
+        start = ways
+        if ways in visited:
+            pass
+
+        print(wtBahnhof)
+        print(visited)
+'''
