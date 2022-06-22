cae0e721dc755621b9a9cecc44b7442e89c895c1
Lukas <lukatran@ksr.ch>
2022-06-22 15:42:18 +0200

    microbit watch, GF

---
+++ microbit_watch.py
@@ -0,0 +1,94 @@
+from microbit import *
+from datetime import datetime
+
+compass.calibrate()
+
+steps=0
+mode = 1
+
+now = datetime.now()
+
+h = now.hour
+
+def on_forever():
+    degrees = input.compass_heading()
+    if degrees < 45:
+        microbit.show("N")
+    elif degrees < 135:
+        microbit.show("E")
+    elif degrees < 225:
+        microbit.show("S")
+    elif degrees < 315:
+        microbit.show("W")
+    else:
+        microbit.scroll("You're lost")
+
+"""
+while True:
+    if accelerometer.was_gesture('shake'):
+        steps += 1
+    if button_a.is_pressed():
+        display.scroll(steps)
+"""
+
+on_forever()
+
+while True:
+    if pin_logo.is_touched:
+        mode += 1
+        if mode
+    if accelerometer.was_gesture('shake'):
+        steps += 1
+    if mode == 1:
+        if h == 1:
+            microbit.display.set_pixel(2,2,9)
+            microbit.display.set_pixel(3,1,9)
+            microbit.display.set_pixel(3,0,9)
+        if h == 2:
+            microbit.display.set_pixel(2,2,9)
+            microbit.display.set_pixel(3,1,9)
+            microbit.display.set_pixel(4,1,9)
+        if h == 3:
+            microbit.display.set_pixel(2,2,9)
+            microbit.display.set_pixel(3,2,9)
+            microbit.display.set_pixel(4,2,9)
+        if h == 4:
+            microbit.display.set_pixel(2,2,9)
+            microbit.display.set_pixel(3,3,9)
+            microbit.display.set_pixel(4,3,9)
+        if h == 5:
+            microbit.display.set_pixel(2,2,9)
+            microbit.display.set_pixel(3,3,9)
+            microbit.display.set_pixel(3,4,9)
+        if h == 6:
+            microbit.display.set_pixel(2,2,9)
+            microbit.display.set_pixel(2,3,9)
+            microbit.display.set_pixel(2,4,9)
+        if h == 7:
+            microbit.display.set_pixel(2,2,9)
+            microbit.display.set_pixel(1,3,9)
+            microbit.display.set_pixel(1,4,9)
+        if h == 8:
+            microbit.display.set_pixel(2,2,9)
+            microbit.display.set_pixel(1,3,9)
+            microbit.display.set_pixel(0,3,9)
+        if h == 9:
+            microbit.display.set_pixel(2,2,9)
+            microbit.display.set_pixel(1,2,9)
+            microbit.display.set_pixel(0,2,9)
+        if h == 10:
+            microbit.display.set_pixel(2,2,9)
+            microbit.display.set_pixel(1,1,9)
+            microbit.display.set_pixel(0,1,9)
+        if h == 11:
+            microbit.display.set_pixel(2,2,9)
+            microbit.display.set_pixel(1,1,9)
+            microbit.display.set_pixel(1,1,9)
+        if h == 12:
+            microbit.display.set_pixel(2,2,9)
+            microbit.display.set_pixel(2,1,9)
+            microbit.display.set_pixel(2,0,9)
+    elif mode == 2:
+        display.scroll(steps)
+    else:
+        on_forever()
