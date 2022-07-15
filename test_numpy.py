6dc01e2d54899376089c070d56fedba48b0b1d05
Lukas <lukatran@ksr.ch>
2022-07-15 09:51:01 +0200

    updated functions

--- test_numpy.py
+++ test_numpy.py
@@ -5,4 +5,15 @@ A = np.array([3,4])
 v = np.array([-1,4])
 
 B = A + -3 * v
+
 print(B)
+
+M = np.array([[3,3],[4,4],[5,5],[5,9]])
+N = np.array([[3,4,6],[5,6,7]])
+print(M.shape)
+
+O = np.dot(M,N)
+
+print(O) 
+print(O.shape)
+#np.dot -> multiplikation
